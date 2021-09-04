fc3d064d6a6b6ab0e82f955a513bb249e34bd3ee
fernando-inf <85811973+fernando-inf@users.noreply.github.com>
2021-09-04 13:26:23 -0300

    Update Void_ButtonD3.ino

--- Display_7_Segments_2_Digits_74HC164/Void_ButtonD3.ino
+++ Display_7_Segments_2_Digits_74HC164/Void_ButtonD3.ino
@@ -2,16 +2,16 @@
 
 Bounce2::Button buttonPin7_upBank = Bounce2::Button();
 
- void button1()
-                {
-                  buttonPin7_upBank.update();
-                  if (buttonPin7_upBank.pressed()) 
-                        {
-                          BankMSB_LSB++; // Incrementa el cambio de Banco
-                          if (BankMSB_LSB > 99) BankMSB_LSB = 0; 
+void button1()
+{
+  buttonPin7_upBank.update();
+  if (buttonPin7_upBank.pressed())
+  {
+    BankMSB_LSB++; // Incrementa el cambio de Banco
+    if (BankMSB_LSB > 99) BankMSB_LSB = 0;
 
-                          DIGITODERECHO++; // Incrementa el cambio de Banco
-                          if (DIGITODERECHO > 9) DIGITODERECHO = 0;
-                        }
+    DIGITODERECHO++; // Incrementa el cambio de Banco
+    if (DIGITODERECHO > 9) DIGITODERECHO = 0;
+  }
 
-                }
+}
