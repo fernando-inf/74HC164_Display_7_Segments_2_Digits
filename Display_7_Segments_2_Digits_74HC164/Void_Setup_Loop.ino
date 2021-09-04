e6e1e53fa29a019b7dd85cfd5a05cc303d38cc67
fernando-inf <85811973+fernando-inf@users.noreply.github.com>
2021-09-04 13:29:12 -0300

    Update Void_Setup_Loop.ino

--- Display_7_Segments_2_Digits_74HC164/Void_Setup_Loop.ino
+++ Display_7_Segments_2_Digits_74HC164/Void_Setup_Loop.ino
@@ -1,18 +1,20 @@
-void setup() 
-            {
-               for (int i=2; i<=7; i++) {pinMode(i, OUTPUT);} 
-               digitalWrite(CLOCK, LOW); 
-               digitalWrite(RESET, HIGH);
-               pinMode(7, INPUT_PULLUP);
+void setup()
+{
+  for (int i = 2; i <= 7; i++) {
+    pinMode(i, OUTPUT);
+  }
+  digitalWrite(CLOCK, LOW);
+  digitalWrite(RESET, HIGH);
+  pinMode(7, INPUT_PULLUP);
 
-               //BOUNCE2
-                          buttonPin7_upBank.attach(7);//D7
-                          buttonPin7_upBank.interval(70);//ms
-                          buttonPin7_upBank.setPressedState(LOW); 
-            }
+  //BOUNCE2
+  buttonPin7_upBank.attach(7);//D7
+  buttonPin7_upBank.interval(70);//ms
+  buttonPin7_upBank.setPressedState(LOW);
+}
 
-void loop() 
-            {
-              Display7S();
-              button1();
-            }
+void loop()
+{
+  Display7S();
+  button1();
+}
